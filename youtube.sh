faf0ff5541c7dbed8a8955602926d4f91ad4259a
Gregory Liras <gregliras@gmail.com>
2012-03-03 23:19:50 +0200

    added bash script to use mplayer to play the video directly

---
+++ youtube.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+youtube_url=$(you-search.py $*)
+echo $youtube_url
+#!/bin/sh
+#
+# Public domain
+# Author: roman [] tsisyk.com
+#
+# Usage: ./me url [youtube-dl parameters]
+#
+
+COOKIE_FILE=/var/tmp/youtube-dl-cookies.txt
+mplayer -cookies -cookies-file ${COOKIE_FILE} $(youtube-dl -g --cookies ${COOKIE_FILE} ${youtube_url})
